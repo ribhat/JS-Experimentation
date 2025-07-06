60e2ba725fb0d65b3e2f0bd01caf44488de7c58b
Rishabh Bhat <Rbhat11@its.jnj.com>
2025-07-06 15:52:36 -0700

    Added mutations to manipulate data from the front end

--- graphql-tutorial/schema.js
+++ graphql-tutorial/schema.js
@@ -32,5 +32,15 @@ export const typeDefs = `#graphql
         author(id:ID!): Author
 
     }
+
+    type Mutation {
+        addGame(game: AddGameInput!): Game
+        deleteGame(id: ID!) : [Game]
+
+    }
+    input AddGameInput {
+        title: String!
+        platform: [String!]!
+    }
 `
 
