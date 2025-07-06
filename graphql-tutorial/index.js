995f3597f6e181828a512c5b3c8354770cb00032
Rishabh Bhat <bhat.rish@gmail.com>
2025-07-05 22:58:30 -0700

    Added first resolver functions

--- graphql-tutorial/index.js
+++ graphql-tutorial/index.js
@@ -3,18 +3,31 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+//db
+import db from './_db.js'
+
 // types
-import { typeDefs } from "./schema";
+import { typeDefs } from './schema.js';
 
 const resolvers = {
-    
+    Query: {
+        games() {
+            return db.games
+        },
+        reviews() {
+            return db.reviews
+        },
+        authors() {
+            return db.authors
+        }
+    }
 }
 
 // server setup
 
 const server = new ApolloServer({
     typeDefs, // type definitions
-    // resolvers - contains resolver functions that allow us to handle incoming reqs and return data to the client
+    resolvers // resolvers - contains resolver functions that allow us to handle incoming reqs and return data to the client
 })
 
 const { url } = await startStandaloneServer(server, {
